918506641d4a66c79d4bd56e26284376bf89f742
Matt Sullivan <mattys940@gmail.com>
2024-08-08 10:10:09 +0100

    Add .gitignore. Change workflow names to differentiate between self-hosted and github-hosted.

--- run.sh
+++ run.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 echo "Launching kubectl container..."
-
-while getopts "b" flag; do
-    case $flag in
-        b) docker build -t kubectl:latest . && clear;;
-    esac
-done
 docker run -it --rm \
     -v $(pwd)/.kube/config:/home/kube-admin/.kube/config \
     -v $(pwd)/deployments/:/home/kube-admin/deployments/ \
     --hostname kubectl-container \
-    kubectl:latest
+    ghcr.io/mjsully/docker-kubectl:latest
